12660721f4bbd1683f83052bcafb9d7e558b1956
Umut Şar <contact@umutsar.net>
2023-07-08 19:10:38 +0300

    Update sar-scanner.py

--- sar-scanner.py
+++ sar-scanner.py
@@ -1,56 +1,67 @@
 import socket
 
-with open("sign.txt", "r") as file:
-    sign = file.read()
-print(sign)
+ascii_code = '''
+ -----------------------------------------@utsr_---------------------------------------
+/  _|      _|  _|      _|  _|      _|  _|_|_|_|_|   _|_|_|_|_|  _|_|_|_|_|  _|_|_|     /
+/  _|      _|  _|_|  _|_|  _|      _|      _|       _|          _|      _|  _|    _|   /
+/  _|      _|  _|  _|  _|  _|      _|      _|       _|_|_|_|_|  _|_|_|_|_|  _|_|_|_|   /
+/  _|      _|  _|      _|  _|      _|      _|               _|  _|      _|  _|  _|     /
+/  _|_|_|_|_|  _|      _|  _|_|_|_|_|      _|       _|_|_|_|_|  _|      _|  _|    _|   /
+/                                                       _|                             /
+ -----------------------------------CREATED BY UMUT SAR-------------------------------
 
-acikPortlar = []
-def port_taramasi(ip, port):
+              *************************sar_scanner*************************
+'''
+print(ascii_code)
+print("Firstly enter target ip.")
+print("Then enter port number(s) or choose one of the existing options.")
+print("Finally choose number for timeout.")
+open_ports = []
+def port_scanning(ip, port):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(taramaHizi)  # Bağlantı zaman aşımını belirleyebilirsiniz
+        sock.settimeout(scanning_speed) 
         result = sock.connect_ex((ip, port))
         
         if result == 0:
-            print(f"Port {port} açık.")
-            acikPortlar.append(port)
+            print(f"Port {port} open.")
+            open_ports.append(port)
 
         else:
-            print(f"Port {port} kapalı.")
+            print(f"Port {port} closed.")
         sock.close()
     except socket.error:
-        print("Bağlantı hatası.")
+        print("Network error!")
 
-ip_adresi = input("Enter IP address: ")
-sik_kullanilan_portlar = [21,22,23,25,53,80,110,115,139,143,161,443,445,514,3306,3389,8080]
-sik_kullanilan_portlar2 = [11,12,13,21]
+ip_address = input("Enter IP address: ")
+commonly_used_port = [21,22,23,25,53,80,110,115,139,143,161,443,445,514,3306,3389,8080]
+commonly_used_port2 = [11,12,13,21]
 
-print(f"Sık kullanılan {len(sik_kullanilan_portlar)} port için 1'i tuşlayınız.")
-print("Sık kullanılan portlar(seviye2) için 2'yi tuşlayınız.")
-print("Tarama aralığı vermek için 3 ü tıklayınız.")
+print(f"Click 1 to select the most commonly used {len(commonly_used_port)} ports.")
+print("Press 2 to select commonly used ports (Level 2).")
+print("Click 3 to set the scanning range.")
 
-girilenPortlar = input("Sayıları girin (virgülle ayırarak): ")
+entered_ports = input("Enter the numbers (separated by commas): ")
+if entered_ports == "1":
+    ports = commonly_used_port
+    print("scanning commonly used ports...")
 
-if girilenPortlar == "1":
-    portlar = sik_kullanilan_portlar
-    print("Sık kullanılan portlar taranıyor...")
+elif entered_ports == "2":
+    ports = commonly_used_port2
+    print("scanning commonly used ports(level 2)...")
 
-elif girilenPortlar == "2":
-    portlar = sik_kullanilan_portlar2
-    print("Sık kullanılan portlar (seviye2) taranıyor")
-
-elif girilenPortlar == "3":
-    print("Port aralığı seçin(seçtikleriniz dahil).")
-    baslangic = input("Kaçtan başlayacak? ")
-    bitis = input("Kaçta bitecek? ")
-    portlar = list(range(int(baslangic),int(bitis) + 1))
+elif entered_ports == "3":
+    print( "Select the port range (including the ones you have chosen).")
+    first_number = input("Starting from which number?")
+    last_number = input("At what number will it end?")
+    ports = list(range(int(first_number),int(last_number) + 1))
 
 else:
-    portlar = list(map(int, girilenPortlar.split(","))) # Taramak istediğiniz portları listeye ekleyin
-taramaHizi = int(input("Port başına düşen tarama saniyesini yazınız(Örneğin 3): "))
+    ports = list(map(int, entered_ports.split(",")))
 
-for port in portlar:
-    port_taramasi(ip_adresi, port)
+scanning_speed = int(input("Enter the scanning seconds per port (ex: 3): "))
+for port in ports:
+    port_scanning(ip_address, port)
 
-print(f"Açık Portlar {acikPortlar}")
-input("İşlemi sonlandırmak için entere basın.")
+print(f"Open ports: {open_ports}")
+input("Press enter to finish process.")
